96ce2ec968dd247273ffbcc87bd951aa133aa85b
delluser <delluser@LAPTOP-6N2GONVC>
2021-05-29 06:54:29 +0530

    test browser first commit

---
+++ script1.js
@@ -0,0 +1,9 @@
+function disp_image1(){
+	document.getElementById('myImage').src = 'cycle.jpg';
+}
+function disp_image2(){
+	document.getElementById('myImage').src = 'motorcycle.jpg';
+}
+function disp_image3(){
+	document.getElementById('myImage').src = 'car.jpg';
+}
